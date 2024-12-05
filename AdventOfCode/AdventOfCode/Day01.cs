63339b63931e3b8365177ea465cdb3f3cf69bca1
Robson <sc.robson@gmail.com>
2024-12-05 09:53:13 +0100

    Fix performance of day 1

--- AdventOfCode/AdventOfCode/Day01.cs
+++ AdventOfCode/AdventOfCode/Day01.cs
@@ -1,16 +1,16 @@
 ﻿public class Day01 : Day
 {
-    public override string Solve1() => List1.Select((itemList1, i) => Math.Abs(itemList1 - List2.ElementAt(i))).Sum().ToString();
+    public override string Solve1() => List1.Select((itemList1, i) => Math.Abs(itemList1 - List2[i])).Sum().ToString();
 
     public override string Solve2() => List1.Sum(itemList1 => itemList1 * List2.Count(itemList2 => itemList2 == itemList1)).ToString();
 
     public Day01()
     {
         var parsedInput = Input.Select(x => x.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(int.Parse));
-        List1 = parsedInput.Select(x => x.ElementAt(0)).Order();
-        List2 = parsedInput.Select(x => x.ElementAt(1)).Order();
+        List1 = parsedInput.Select(x => x.ElementAt(0)).Order().ToArray();
+        List2 = parsedInput.Select(x => x.ElementAt(1)).Order().ToArray();
     }
 
-    private IEnumerable<int> List1 { get; }
-    private IEnumerable<int> List2 { get; }
+    private int[] List1 { get; }
+    private int[] List2 { get; }
 }
