4a8a4640e37d334ac65c3f60a9468317fd4d37b3
Robson <sc.robson@gmail.com>
2024-12-18 10:22:00 +0100

    Refactor and fix day 14

--- AdventOfCode/AdventOfCode/Day14.cs
+++ AdventOfCode/AdventOfCode/Day14.cs
@@ -5,39 +5,40 @@ public class Day14 : Day
 {
     public override string Solve1()
     {
+        var robots = GetRobots().ToArray();
+
         for (int seconds = 0; seconds < 100; seconds++)
         {
-            foreach (var robot in Robots)
+            foreach (var robot in robots)
             {
                 robot.Move(Border);
             }
         }
 
-        var topLeftCount = Robots.Where(r => r.Position.X < Center.X && r.Position.Y < Center.Y).Count();
-        var topRightCount = Robots.Where(r => r.Position.X > Center.X / 2 && r.Position.Y < Center.Y).Count();
-        var bottomLeftCount = Robots.Where(r => r.Position.X < Center.X / 2 && r.Position.Y > Center.Y).Count();
-        var bottomRightCount = Robots.Where(r => r.Position.X > Center.X / 2 && r.Position.Y > Center.Y).Count();
+        var topLeftCount = robots.Where(r => r.Position.X < Center.X && r.Position.Y < Center.Y).Count();
+        var topRightCount = robots.Where(r => r.Position.X > Center.X && r.Position.Y < Center.Y).Count();
+        var bottomLeftCount = robots.Where(r => r.Position.X < Center.X && r.Position.Y > Center.Y).Count();
+        var bottomRightCount = robots.Where(r => r.Position.X > Center.X && r.Position.Y > Center.Y).Count();
 
-        return (topLeftCount * bottomLeftCount * topRightCount * bottomRightCount).ToString();
+        return (topLeftCount * topRightCount * bottomLeftCount * bottomRightCount).ToString();
     }
 
     public override string Solve2()
     {
-        ResetRobots();
-
         int seconds = 0;
         var diagonal = Enumerable.Range(1, 6).Select(x => new Vector2(x, -x)).ToArray();
+        var robots = GetRobots().ToArray();
 
         while (true)
         {
             seconds++;
 
-            foreach (var robot in Robots)
+            foreach (var robot in robots)
             {
                 robot.Move(Border);
             }
 
-            var hashset = Robots.Select(r => r.Position).ToHashSet();
+            var hashset = robots.Select(r => r.Position).ToHashSet();
 
             foreach (var position in hashset)
             {
@@ -53,29 +54,25 @@ public class Day14 : Day
     {
         Border = new(100, 102);
         Center = new(Border.X / 2, Border.Y / 2);
-        ResetRobots();
     }
 
-    private void ResetRobots()
+    private IEnumerable<Robot> GetRobots()
     {
         var regex = new Regex("p=(-?\\d+),(-?\\d+)\\s+v=(-?\\d+),(-?\\d+)");
-
-        Robots = new List<Robot>();
 
         foreach (var line in Input)
         {
             var matches = regex.Match(line);
 
-            Robots.Add(new Robot(
+            yield return new Robot(
                 position: new Vector2(int.Parse(matches.Groups[1].Value), int.Parse(matches.Groups[2].Value)),
                 speed: new Vector2(int.Parse(matches.Groups[3].Value), int.Parse(matches.Groups[4].Value))
-            ));
+            );
         }
     }
 
     private Vector2 Border { get; }
     private Vector2 Center { get; }
-    private List<Robot> Robots { get; set; } = [];
 
     private class Robot(Vector2 position, Vector2 speed)
     {
@@ -106,4 +103,3 @@ public class Day14 : Day
         }
     }
 }
-
