795a45bc2406c9560048babfed68c845b69b31de
Robson <sc.robson@gmail.com>
2024-12-24 00:34:05 +0100

    Solution day 13 part 2 and refactor

--- AdventOfCode/AdventOfCode/Day13.cs
+++ AdventOfCode/AdventOfCode/Day13.cs
@@ -1,90 +1,54 @@
-﻿using System.Numerics;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 public class Day13 : Day
 {
-    public override string Solve1() => Machines.Select(m => Play(m, new Vector2(0, 0), Memo)).Where(x => x < int.MaxValue).Sum().ToString();
+    public override string Solve1() => Machines.Sum(Play).ToString();
 
-    public override string Solve2() => Machines.Select(m => Play(m, new Vector2(0, 0), Memo)).Where(x => x < int.MaxValue).Sum().ToString();
+    public override string Solve2() => HarderMachines.Sum(Play).ToString();
 
-    private int Play(Machine machine, Vector2 position, Dictionary<(Vector2, int), int> memo, int cost = 0, int cheapest = int.MaxValue)
+    private long Play(Machine machine)
     {
-        if (memo.TryGetValue((position, cost), out var cached))
+        long ax = machine.ButtonA.X, bx = machine.ButtonB.X, px = machine.Prize.X;
+        long ay = machine.ButtonA.Y, by = machine.ButtonB.Y, py = machine.Prize.Y;
+        long countB = (py * ax - ay * px) / (ax * by - ay * bx);
+        long countA = (px - bx * countB) / ax;
+
+        if (machine.Prize.X == machine.ButtonA.X * countA + machine.ButtonB.X * countB && machine.Prize.Y == machine.ButtonA.Y * countA + machine.ButtonB.Y * countB)
         {
-            return cached;
-        }
-        
-        if (position == machine.Prize)
-        {
-            return Math.Min(cost, cheapest);
-        }
-        
-        if (position.X > machine.Prize.X || position.Y > machine.Prize.Y)
-        {
-            return Math.Min(int.MaxValue, cheapest);
+            return countA * 3 + countB;
         }
 
-        cheapest = Play(machine, position + machine.ButtonA, memo, cost + 3, cheapest);
-        cheapest = Play(machine, position + machine.ButtonB, memo, cost + 1, cheapest);
-
-        memo[(position, cost)] = cheapest;
-
-        return cheapest;
+        return 0;
     }
-
-    private int PlayHard(Machine machine, Vector2 position, Dictionary<(Vector2, int), int> memo, int cost = 0, int cheapest = int.MaxValue)
-    {
-        if (memo.TryGetValue((position, cost), out var cached))
-        {
-            return cached;
-        }
-
-        var gcdOfX = BigInteger.GreatestCommonDivisor((long)machine.Prize.X, (long)position.X);
-        var gcdOfY = BigInteger.GreatestCommonDivisor((long)machine.Prize.Y, (long)position.Y);
-
-        if (gcdOfX > 1 && gcdOfY > 1)
-        {
-            return Math.Min(cost, cheapest);
-        }
-
-        cheapest = Play(machine, position + machine.ButtonA, memo, cost + 3, cheapest);
-        cheapest = Play(machine, position + machine.ButtonB, memo, cost + 1, cheapest);
-
-        memo[(position, cost)] = cheapest;
-
-        return cheapest;
-    }
-
 
     public Day13()
     {
         var buttonRegex = new Regex("([A-Z])\\+(\\d+)");
-        var priceRegex = new Regex("([A-Z])\\=(\\d+)");
+        var prizeRegex = new Regex("([A-Z])\\=(\\d+)");
 
         for (int i = 0; i < Input.Count(); i = i + 4)
         {
             var buttonA = buttonRegex.Matches(Input.ElementAt(i));
             var buttonB = buttonRegex.Matches(Input.ElementAt(i + 1));
-            var prize = priceRegex.Matches(Input.ElementAt(i + 2));
+            var prize = prizeRegex.Matches(Input.ElementAt(i + 2));
 
             Machines.Add(new Machine(
-                ButtonA: new Vector2(int.Parse(buttonA[0].Groups[2].Value), int.Parse(buttonA[1].Groups[2].Value)),
-                ButtonB: new Vector2(int.Parse(buttonB[0].Groups[2].Value), int.Parse(buttonB[1].Groups[2].Value)),
-                Prize: new Vector2(int.Parse(prize[0].Groups[2].Value), int.Parse(prize[1].Groups[2].Value))
+                ButtonA: new LongVector2(int.Parse(buttonA[0].Groups[2].Value), int.Parse(buttonA[1].Groups[2].Value)),
+                ButtonB: new LongVector2(int.Parse(buttonB[0].Groups[2].Value), int.Parse(buttonB[1].Groups[2].Value)),
+                Prize: new LongVector2(int.Parse(prize[0].Groups[2].Value), int.Parse(prize[1].Groups[2].Value))
             ));
 
             HarderMachines.Add(new Machine(
-                ButtonA: new Vector2(int.Parse(buttonA[0].Groups[2].Value), int.Parse(buttonA[1].Groups[2].Value)),
-                ButtonB: new Vector2(int.Parse(buttonB[0].Groups[2].Value), int.Parse(buttonB[1].Groups[2].Value)),
-                Prize: new Vector2(int.Parse(prize[0].Groups[2].Value) * 10000000000000, int.Parse(prize[1].Groups[2].Value) * 10000000000000)
+                ButtonA: new LongVector2(int.Parse(buttonA[0].Groups[2].Value), int.Parse(buttonA[1].Groups[2].Value)),
+                ButtonB: new LongVector2(int.Parse(buttonB[0].Groups[2].Value), int.Parse(buttonB[1].Groups[2].Value)),
+                Prize: new LongVector2(int.Parse(prize[0].Groups[2].Value) + 10000000000000, int.Parse(prize[1].Groups[2].Value) + 10000000000000)
             ));
         }
     }
 
     private List<Machine> Machines { get; } = new();
     private List<Machine> HarderMachines { get; } = new();
-    private Dictionary<(Vector2, int), int> Memo => new();
-
-    private record Machine(Vector2 ButtonA, Vector2 ButtonB, Vector2 Prize);
+    private record Machine(LongVector2 ButtonA, LongVector2 ButtonB, LongVector2 Prize);
+    private record LongVector2(long X, long Y);
 }
 
