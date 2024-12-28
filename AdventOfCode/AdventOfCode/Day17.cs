439f494a3ca8cf630cee8f84eda80e2b24e6d0e0
Robson <sc.robson@gmail.com>
2024-12-28 01:56:21 +0100

    Solution day 17 part 2

--- AdventOfCode/AdventOfCode/Day17.cs
+++ AdventOfCode/AdventOfCode/Day17.cs
@@ -6,9 +6,31 @@
         return string.Join(',', Run(registers, program));
     }
 
-    public override string Solve2() => 0.ToString();
+    public override string Solve2()
+    {
+        var (registers, program) = Initialize();
 
-    private List<long> Run(Dictionary<char, long> registers, long[] program, bool haltWhenOutputDoesntMatchProgram = false)
+        for (long a = 1; a <= long.MaxValue; a++)
+        {
+            (registers['A'], registers['B'], registers['C']) = (a, 0, 0);
+
+            var output = Run(registers, program);
+
+            if (output.TakeLast(output.Count).SequenceEqual(program.TakeLast(output.Count)))
+            {
+                if (output.Count == program.Length)
+                {
+                    return a.ToString();
+                }
+
+                a = (8 * a) - 1;
+            }
+        }
+
+        throw new Exception();
+    }
+
+    private List<long> Run(Dictionary<char, long> registers, long[] program)
     {
         var pointer = 0L;
         var output = new List<long>();
@@ -42,11 +64,6 @@
             else if (program[pointer] == 5)
             {
                 output.Add(GetComboOperandValue(program[pointer + 1], registers) % 8);
-
-                if (haltWhenOutputDoesntMatchProgram && !Enumerable.SequenceEqual(output, program.Take(output.Count)))
-                {
-                    return output;
-                }
             }
             else if (program[pointer] == 6)
             {
@@ -76,24 +93,23 @@
         _ => throw new NotImplementedException()
     };
 
-    private int PerformDivision(Dictionary<char, long> registers, long[] program, long pointer)
+    private long PerformDivision(Dictionary<char, long> registers, long[] program, long pointer)
     {
         var numerator = registers['A'];
         var denominator = (long)Math.Pow(2, GetComboOperandValue(program[pointer + 1], registers));
 
-        return denominator > 0 ? (int)Math.Min(int.MaxValue, numerator / denominator) : 0;
+        return denominator > 0 ? (long)Math.Min(long.MaxValue, numerator / denominator) : 0;
     }
 
     private (Dictionary<char, long> registers, long[] program) Initialize()
     {
         var registers = new Dictionary<char, long>()
         {
-            { 'A', int.Parse(Input.ElementAt(0).Split(':').Last().Trim()) },
-            { 'B', int.Parse(Input.ElementAt(1).Split(':').Last().Trim()) },
-            { 'C', int.Parse(Input.ElementAt(2).Split(':').Last().Trim()) }
+            { 'A', long.Parse(Input.ElementAt(0).Split(':').Last().Trim()) },
+            { 'B', long.Parse(Input.ElementAt(1).Split(':').Last().Trim()) },
+            { 'C', long.Parse(Input.ElementAt(2).Split(':').Last().Trim()) }
         };
 
         return (registers, Input.ElementAt(4).Split(':').Last().Trim().Split(',').Select(long.Parse).ToArray());
     }
 }
-
