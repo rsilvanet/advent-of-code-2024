abe4e10f308f4188465d7b5d596ce5d663c7600b
Robson <sc.robson@gmail.com>
2024-12-31 14:25:56 +0100

    Solution day 24 part 2

--- AdventOfCode/AdventOfCode/Day24.cs
+++ AdventOfCode/AdventOfCode/Day24.cs
@@ -3,7 +3,29 @@
     public override string Solve1()
     {
         var (wires, gates) = ReadInput();
+        Process(wires, gates);
+        return ReadWireValue(wires, "z").ToString();
+    }
 
+    public override string Solve2()
+    {
+        var (wires, gates) = ReadInput();
+
+        var zGates = gates.Where(g => g.Output.StartsWith('z')).OrderBy(g => g.Output).SkipLast(1);
+        var nonEdgeGates = gates.Where(x => !x.Input1.StartsWith("x") && !x.Input1.StartsWith("y") && !x.Output.StartsWith("z"));
+        var nonZeroGates = gates.Where(x => !x.Input1.EndsWith("00"));
+
+        var zGatesNotFedByXor = zGates.Where(x => x.Operation != "XOR");
+        var nonEdgeGatesFedByXor = nonEdgeGates.Where(x => x.Operation == "XOR");
+        var xorGatesFeedingOr = nonZeroGates.Where(c => c.Operation == "XOR" && gates.Any(n => IsFeedingGate(c, n) && n.Operation == "OR"));
+        var andGatesNotFeedingOr = nonZeroGates.Where(x => x.Operation == "AND" && gates.Any(y => IsFeedingGate(x, y) && y.Operation != "OR"));
+        var badGates = zGatesNotFedByXor.Concat(nonEdgeGatesFedByXor).Concat(xorGatesFeedingOr).Concat(andGatesNotFeedingOr).Distinct();
+
+        return string.Join(",", badGates.Select(g => g.Output).Order());
+    }
+
+    private void Process(Dictionary<string, int?> wires, List<Gate> gates)
+    {
         while (wires.Any(x => x.Key.StartsWith("z") && x.Value is null))
         {
             foreach (var gate in gates.Where(g => wires[g.Input1].HasValue && wires[g.Input2].HasValue && !wires[g.Output].HasValue))
@@ -17,16 +39,22 @@
                 };
             }
         }
-
-        return Convert.ToInt64(string.Join("", wires.Where(x => x.Key.StartsWith("z")).OrderByDescending(x => x.Key).Select(x => x.Value)), 2).ToString();
     }
 
-    public override string Solve2() => 0.ToString();
+    private long ReadWireValue(Dictionary<string, int?> wires, string letter)
+    {
+        var relevantWires = wires.Where(x => x.Key.StartsWith(letter)).OrderByDescending(x => x.Key).Select(x => x.Value);
+        var binaryNumber = string.Join("", relevantWires);
+
+        return Convert.ToInt64(binaryNumber, 2);
+    }
+
+    private bool IsFeedingGate(Gate current, Gate next) => current.Output == next.Input1 || current.Output == next.Input2;
 
     private (Dictionary<string, int?> Wires, List<Gate> Gates) ReadInput()
     {
-        var wires = new Dictionary<string, int?>();
         var gates = new List<Gate>();
+        var wires = new Dictionary<string, int?>();
 
         foreach (var line in Input)
         {
@@ -51,4 +79,3 @@
 
     private record Gate(string Input1, string Operation, string Input2, string Output);
 }
-
